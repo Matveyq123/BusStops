b671394c8daf14c3c2ad763084f90eb4ace75ffc
Matvey Korobieinikov <53523153+Matveyq123@users.noreply.github.com>
2021-03-09 17:31:34 +0200

    bus_manager.h

--- bus_manager.h
+++ bus_manager.h
@@ -2,13 +2,13 @@
 #include "responses.h"
 class BusManager {
 public:
-    void AddBus(const string& bus, const vector<string>& stops);
+    void AddBus(const string& bus, const vector<string>& stops); //Add new bus with stops
 
-BusesForStopResponse GetBusesForStop(const string& stop) const;
+BusesForStopResponse GetBusesForStop(const string& stop) const; //Print the names of all bus routes passing through the stop.
 
-StopsForBusResponse GetStopsForBus(const string& bus) const;
+StopsForBusResponse GetStopsForBus(const string& bus) const; // Print the names of all stops on the bus route with a list of buses that you can change to at each of the stops.
 
-AllBusesResponse GetAllBuses() const;
+AllBusesResponse GetAllBuses() const; // Print list of all routes with stops.
 
 private:
     map<string, vector<string>> buses_to_stops;
